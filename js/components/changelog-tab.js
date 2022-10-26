c600949b0b3fc7ad3fb0cf8edde7d01f676a6e38
Nif <73426353+TJCgames@users.noreply.github.com>
2022-10-26 18:56:48 +0100

    Update changelog-tab.js

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -1,8 +1,11 @@
 Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
-    <template v-slot:title>v1.0.0</template>
-    <template v-slot:text>When modding, edit this when you make new releases!
+    <template v-slot:title>v0.0.2</template>
+    <template v-slot:text>- Add a lot of achivements
+    </template>
+    <template v-slot:title>v0.0.1</template>
+    <template v-slot:text>- Add 6 extra infinities<br>- Add area<br>- Add some achivements
     </template>
     </guide-item>
 </div>`
