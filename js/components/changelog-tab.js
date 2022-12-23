be441f83e31e7b6a822cd62ecd8e67d9a2fe8535
Nif <73426353+Unexian@users.noreply.github.com>
2022-12-23 14:30:59 +0000

    Update changelog-tab.js

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -1,6 +1,9 @@
 Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
+    <template v-slot:title>v0.0.3</template>
+    <template v-slot:text>- Added Russian layer names<br>- Fixed some achivements that didnt work as intended
+    </template>
     <template v-slot:title>v0.0.2</template>
     <template v-slot:text>- Add a lot of achivements
     </template>
