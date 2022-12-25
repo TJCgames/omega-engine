87326698f3f805d5f9db341a9d9cd03ed7a483fe
Nif <73426353+Unexian@users.noreply.github.com>
2022-12-25 19:13:58 +0000

    0.0.4k - remove subversions

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -1,21 +1,6 @@
 Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
-    <template v-slot:title>v0.0.4j</template>
-    <template v-slot:text>- Change the version number from the default to the actual version number<br>- Be confused about why it took so long
-    </template>
-    </guide-item>
-    <guide-item>
-    <template v-slot:title>v0.0.4i</template>
-    <template v-slot:text>- Hopefully fix changelog tab
-    </template>
-    </guide-item>
-    <guide-item>
-    <template v-slot:title>v0.0.4b-h</template>
-    <template v-slot:text>- Fix layer names
-    </template>
-    </guide-item>
-    <guide-item>
     <template v-slot:title>v0.0.4</template>
     <template v-slot:text>- More colours to layer names<br>- Compacted layer names (hope this works)
     </template>
