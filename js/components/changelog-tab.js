872e439813e7b50c6aa90062f13bd04a1d5a710d
TJCgames <minebloxdash@gmail.com>
2023-12-29 21:20:52 +0000

    changelog

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -1,6 +1,13 @@
 Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
+    <template v-slot:title>v0.0.2</template>
+    <template v-slot:text>
+        - Added framework for a layer working on the number of layers you have
+        - Pushed restack back to layer 25 (Α)
+    </template>
+    </guide-item>
+    <guide-item>
     <template v-slot:title>v0.0.1</template>
     <template v-slot:text>
         - Added volatility automator
