a3a8761838d9ec2d2715d6cadb4b04c3c82df64b
TJCgames <minebloxdash@gmail.com>
2023-12-29 16:50:57 +0000

    Ugh fine

--- js/mod.js
+++ js/mod.js
@@ -3,7 +3,7 @@ const mod = {
     secondaryName: "Layers",
     version: "0.0.1",
     engineVer: "1.0.1", //DO NOT MODIFY
-    debugMode: true,
+    debugMode: false,
     Infinities: [new Decimal(2).pow(1024), new Decimal("1.8e30008"), new Decimal("1.8e300000008"), new Decimal("ee38")],
     themes: [
         ["Dark", "css/themes/dark.css"],
