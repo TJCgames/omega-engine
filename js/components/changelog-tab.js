03417522bab6b4db92f32af4749dfb68886d2ae0
TJCgames <minebloxdash@gmail.com>
2023-12-30 14:10:34 +0000

    <br>

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -3,22 +3,22 @@ Vue.component("changelog-tab", {
     <guide-item>
     <template v-slot:title>v0.1.0</template>
     <template v-slot:text>
-        - ADDED A FRACTAL UPGRADE!!!!!
-        - The gimmick with fractal upgrades is that buying them actually removes layers from you, however they also provide massive bonuses
+        - ADDED A FRACTAL UPGRADE!!!!!<br>
+        - The gimmick with fractal upgrades is that buying them actually removes layers from you, however they also provide massive bonuses<br>
         - Still needs balancing
     </template>
     </guide-item>
     <guide-item>
     <template v-slot:title>v0.0.2</template>
     <template v-slot:text>
-        - Added framework for a layer working on the number of layers you have
+        - Added framework for a layer working on the number of layers you have<br>
         - Pushed restack back to layer 25 (Α)
     </template>
     </guide-item>
     <guide-item>
     <template v-slot:title>v0.0.1</template>
     <template v-slot:text>
-        - Added volatility automator
+        - Added volatility automator<br>
         - Made all automators on by default
     </template>
     </guide-item>
